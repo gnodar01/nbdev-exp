38b9400e8f75ca368629fa75694cfa2a329bae6f
Nodar Gogoberidze <gnodar01@gmail.com>
2025-04-09 10:12:58 -0400

    Run jupytext --sync nbs/*.ipynb

---
+++ jpt/00_core.py
@@ -0,0 +1,35 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.0
+#   kernelspec:
+#     display_name: python3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # core
+#
+# > Fill in a module description here
+
+# %%
+#| default_exp core
+
+# %%
+#| hide
+from nbdev.showdoc import *
+
+
+# %%
+#| export
+def foo(): pass
+
+
+# %%
+#| hide
+import nbdev; nbdev.nbdev_export()
---
+++ jpt/index.py
@@ -0,0 +1,100 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.0
+#   kernelspec:
+#     display_name: python3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+#| hide
+from nbdev_exp.core import *
+
+# %% [markdown]
+# # nbdev-exp
+#
+# > An experiment with nvdev
+
+# %% [markdown]
+# This file will become your README and also the index of your documentation.
+
+# %% [markdown]
+# ## Developer Guide
+
+# %% [markdown]
+# If you are new to using `nbdev` here are some useful pointers to get you started.
+
+# %% [markdown]
+# ### Install nbdev_exp in Development mode
+
+# %% [markdown]
+# ```sh
+# # make sure nbdev_exp package is installed in development mode
+# $ pip install -e .
+#
+# # make changes under nbs/ directory
+# # ...
+#
+# # compile to have changes apply to nbdev_exp
+# $ nbdev_prepare
+# ```
+
+# %% [markdown]
+# ## Usage
+
+# %% [markdown]
+# ### Installation
+
+# %% [markdown]
+# Install latest from the GitHub [repository][repo]:
+#
+# ```sh
+# $ pip install git+https://github.com/gnodar01/nbdev-exp.git
+# ```
+#
+# or from [conda][conda]
+#
+# ```sh
+# $ conda install -c gnodar01 nbdev_exp
+# ```
+#
+# or from [pypi][pypi]
+#
+#
+# ```sh
+# $ pip install nbdev_exp
+# ```
+#
+#
+# [repo]: https://github.com/gnodar01/nbdev-exp
+# [docs]: https://gnodar01.github.io/nbdev-exp/
+# [pypi]: https://pypi.org/project/nbdev-exp/
+# [conda]: https://anaconda.org/gnodar01/nbdev-exp
+
+# %% [markdown]
+# ### Documentation
+
+# %% [markdown]
+# Documentation can be found hosted on this GitHub [repository][repo]'s [pages][docs]. Additionally you can find package manager specific guidelines on [conda][conda] and [pypi][pypi] respectively.
+#
+# [repo]: https://github.com/gnodar01/nbdev-exp
+# [docs]: https://gnodar01.github.io/nbdev-exp/
+# [pypi]: https://pypi.org/project/nbdev-exp/
+# [conda]: https://anaconda.org/gnodar01/nbdev-exp
+
+# %% [markdown]
+# ## How to use
+
+# %% [markdown]
+# Fill me in please! Don't forget code examples:
+
+# %%
+1+1
+
+# %%
